08f925661a0b856616c51beb521f9c18b3496bd7
aldrud <147627044+aldrud@users.noreply.github.com>
2023-10-12 15:23:06 +0900

    Github Desktop
    Github Desktop

---
+++ Lab1-2.py
@@ -0,0 +1,18 @@
+#Bubble sorting
+
+def bubbleSort(list):
+    n = len(list)
+
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if list[j] > list[j+1] :
+                list[j], list[j+1] = list[j+1], list[j]
+
+
+list = [64, 34, 25, 12, 22, 11, 90]
+print("Orginal list = %s"%list)
+
+bubbleSort(list)
+
+print("Sorted list by Bubble method")
+print(list)
